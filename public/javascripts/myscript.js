50afc72ce0c7de208ad13dd10e8477f87260fdda
Zoe <33168975+ZoeLeee@users.noreply.github.com>
2018-04-14 16:18:02 +0800

    sign

--- public/javascripts/myscript.js
+++ public/javascripts/myscript.js
@@ -20,46 +20,92 @@ angular.module('FunBooks', ['ngRoute'])
   })
   .component('sign', {
     template: `
-    <ul class="li-inline flex-space-between">
-    <li  class="sign">
-      <a href="#!/login">sign in</a>
-    </li>
-    <li class="cart">
-      <a href="#!/cart">xx in Cart</a>
-    </li>
-  </ul>`
-  })
-  .controller('BookDetailController', function () {
-    this.book = {
-      title: "hha",
-      author: "zoe",
-      price: "123",
-      publisher: "123",
-      publishDate: Date.now().toString(),
-      bookDescription: "hehe"
+      <ul class="li-inline flex-space-between">
+      <li  class="sign">
+        <a href="#!/login" ng-hide={{$ctrl.isLogin}}>sign in</a>
+        <div  ng-show={{$ctrl.isLogin}}>
+          <span>{{$ctrl.uname}}</span>
+          <a href="" class='underline' ng-click=$ctrl.signout()>(sign out)</a> 
+        </div> 
+      </li>
+      <li class="cart">
+        <a href="#!/cart">{{$ctrl.totalNum}} in Cart</a>
+      </li>
+    </ul>`,
+    controller($http) {
+      this.uname = sessionStorage.getItem('uname');
+      this.isLogin = new Boolean(this.uname);
+      this.totalNum = sessionStorage.getItem('totalNum');
+      this.signout = () => {
+        $http.get(`http://localhost:3000/signout`)
+          .then(res => {
+            console.log(res);
+          })
+        sessionStorage.clear();
+        this.uname = "";
+        this.isLogin = false;
+      }
     }
-  }
-  )
+  })
+  .controller('BookDetailController', function ($routeParams, $http) {
+    let bookId = $routeParams.bookId;
+    this.currentPage = $routeParams.page;
+    $http
+      .get(`http://localhost:3000/loadbook/` + bookId)
+      .then(res => {
+        if (res.data.success) {
+          this.book = res.data.data[0]
+        } else console.log("链接失败");
+      })
+
+    this.goback = () => {
+      history.go(-1);
+    }
+  })
+  .controller('LoginController', function ($http) {
+    this.uname = "";
+    this.pwd = "";
+    this.isFail = false;
+    this.signto = () => {
+      if (this.uname && this.pwd) {
+        $http
+          .post(`http://localhost:3000/signin`, { name: this.uname, pwd: this.pwd })
+          .then(res => {
+            if (res.data.success) {
+              let data = res.data.data;
+              if (data.length) {
+                console.log(res.data);
+                sessionStorage.uname = data[0].name;
+                sessionStorage.totalNum = data[0].totalnum;
+                history.go(-1);
+                this.isFail = false;
+              } else {
+                this.isFail = true;
+              }
+            } else console.log("链接失败");
+          })
+      } else alert("请输入用户名和密码")
+    }
+  })
   .controller('HomeController', function ($http, $scope) {
     document.querySelectorAll('#pageLeft ul>li')[0].className = 'active';
     this.showBooks = [];
     this.page = 1;
     this.range = [];
     this.category = "Computer";
+
     this.loadPage = (category, page) => {
       this.page = page;
-      console.log(page);
       document.querySelectorAll('#pageLeft ul>li')[0].className = '';
       this.category = category;
       $http
         .get(`http://localhost:3000/loadpage?category=${category}&page=${page}`)
         .then(response => {
           let status = response.data.success;
-          this.book = response.data.data
+          this.showBooks = response.data.data;
+          let totalCount = response.data.totalCount;
           if (status) {
-            this.totalPage = Math.ceil(this.book.length / 8);
-            this.showBooks.length = 0;
-            this.showBooks.unshift(...this.book.slice((this.page - 1) * 8, (this.page - 1) * 8 + 8));
+            this.totalPage = Math.ceil(totalCount / 8);
             this.setPageRange();
           } else console.log("数据请求失败");
         });
@@ -80,9 +126,9 @@ angular.module('FunBooks', ['ngRoute'])
         this.page = this.totalPage
       } else if (this.page < 1) {
         this.page = 1;
+      } else {
+        this.loadPage(this.category, this.page);
       }
-      this.showBooks.length = 0;
-      this.showBooks.unshift(...this.book.slice((this.page - 1) * 8, (this.page - 1) * 8 + 8));
     }
   }
   )
@@ -94,15 +140,15 @@ angular.module('FunBooks', ['ngRoute'])
         controller: 'HomeController',
         controllerAs: 'hmCtrl',
       })
-      .when('/bookdetail', {
+      .when('/bookdetail/:bookId/:page', {
         templateUrl: "../javascripts/detail.html",
         controller: 'BookDetailController',
         controllerAs: 'bdCtrl'
       })
       .when('/login', {
         templateUrl: "../javascripts/login.html",
-        controller: '',
-        controllerAs: ''
+        controller: 'LoginController',
+        controllerAs: 'lgCtrl'
       }).
       when('/cart', {
         templateUrl: "../javascripts/bookCart.html",
--- routes/myroutes.js
+++ routes/myroutes.js
@@ -1,95 +1,174 @@
 const express = require('express');
 const router = express.Router();
-const MongoClient=require('mongodb').MongoClient;
-const assert=require('assert');
-const url=require('url');
+const MongoClient = require('mongodb').MongoClient;
+const assert = require('assert');
+const url = require('url');
+const ObjectID=require('mongodb').ObjectID;
 // 数据连接数据
-const Urls=`mongodb://localhost:27017/assignment2`;
+const Urls = `mongodb://localhost:27017/assignment2`;
 
 
-router.get('/loadpage', function(req, res, next) {
-  let page=req.query.page;
-  handleDb(req,res,"bookCollection",{category:req.query.category},findData,data=>{
-    console.log(data);
-    res.end(`{"success":true,"data":${JSON.stringify(data)}}`);
-  })
+router.get('/loadpage', function (req, res, next) {
+    let page = parseInt(req.query.page);
+    findByPagination("bookCollection", { category: req.query.category }, page, 8, data => {
+        res.end(JSON.stringify(data));
+    })
 });
 
+router.get('/loadbook/:bookId',function(req,res,next){
+    let bookId=req.params.bookId.slice(1);
+    handleDb("bookCollection", {_id:new ObjectID(bookId)}, find, data=>{
+        res.end(JSON.stringify(data));
+    })
+
+})
+
+router.post('/signin',function(req,res,next){
+    let name=req.body.name;
+    let password=req.body.pwd;
+    handleDb("userCollection", {name,password}, find, data=>{
+        if(data.data.length){
+            data.msg="登陆成功";
+            handleDb("userCollection", [{name,password},{$set:{"status":"Online"}}], update, res=>{
+                console.log(res);
+            })
+            req.session.userId=data.data[0]._id;
+            res.end(JSON.stringify(data));
+        }else{
+            data.msg="登陆失败";
+            res.end(JSON.stringify(data));
+        }
+    })
+
+})
+
+router.get('/signout',function(req,res,next){
+    if(req.session.userId){
+        handleDb("userCollection", [{_id:new ObjectID(req.session.userId)},{$set:{"status":"Offline"}}], update, data=>{
+            console.log(data);
+            res.end(JSON.stringify(data));
+        })
+    }
+    req.session.userId="";
+})
+/************数据库操作函数*************/
 //add一条数据 
 function add(db, collections, selector, fn) {
-  let collection = db.collection(collections);
-  collection.insertMany([selector], function (err, result) {
-      try {
-          assert.equal(err, null)
-      } catch (e) {
-          console.log(e);
-          result = [];
-      };
+    let collection = db.collection(collections);
+    collection.insertMany([selector], function (err, result) {
+        try {
+            assert.equal(err, null)
+        } catch (e) {
+            console.log(e);
+            result = [];
+        };
 
-      fn(result);
-      db.close();
-  });
+        fn(result);
+        db.close();
+    });
 };
 
 //delete
 function deletes(db, collections, selector, fn) {
-  let collection = db.collection(collections);
-  collection.deleteOne(selector, function (err, result) {
-      try {
-          assert.equal(err, null);
-          assert.notStrictEqual(0, result.result.n);
-      } catch (e) {
-          console.log(e);
-          result.result = "";
-      };
+    let collection = db.collection(collections);
+    collection.deleteOne(selector, function (err, result) {
+        try {
+            assert.equal(err, null);
+            assert.notStrictEqual(0, result.result.n);
+        } catch (e) {
+            console.log(e);
+            result.result = "";
+        };
 
-      fn(result.result ? [result.result] : []); //如果没报错且返回数据不是0，那么表示操作成功。
-      db.close;
-  });
+        fn(result.result ? [result.result] : []); //如果没报错且返回数据不是0，那么表示操作成功。
+        db.close;
+    });
 };
 
 //find
-function findData(db, collections, selector, fn) {
+function find(db, collections, selector, fn) {
     let collection = db.collection(collections);
     collection.find(selector).toArray(function (err, result) {
         try {
             assert.equal(err, null);
+            fn({
+                success:true,
+                data:result
+            });
         } catch (e) {
             console.log(e);
-            result = [];
+            fn({
+                success:false,
+                data:[]
+            });
         }
-        db.close();
-        fn(result);
     });
 
 }
 //update
-function updates (db, collections, selector, fn) {
-  let collection = db.collection(collections);
+function update(db, collections, selector, fn) {
+    let collection = db.collection(collections);
+    collection.updateOne(selector[0], selector[1], function (err, result) {
+        try {
+            assert.equal(err, null);
+            assert.notStrictEqual(0, +result.result.n);
+        } catch (e) {
+            console.log(e);
+            result.result = "";
+        };
 
-  collection.updateOne(selector[0], selector[1], function (err, result) {
-      try {
-          assert.equal(err, null);
-          assert.notStrictEqual(0, result.result.n);
-      } catch (e) {
-          console.log(e);
-          result.result = "";
-      };
-
-      fn(result.result ? [result.result] : []); //如果没报错且返回数据不是0，那么表示操作成功。
-      db.close();
-  });
+        fn(result.result ? [result.result] : []); //如果没报错且返回数据不是0，那么表示操作成功。
+        db.close();
+    });
 
 }
 
-function handleDb(req, res, collections, selector, handleFun,fn) {
-  MongoClient.connect(Urls, function (err, db) {
-      assert.equal(null, err);
-      console.log("Connected correctly to server");
-      // 根据 请求的地址来确定是什么操作  （为了安全，避免前端直接通过请求url操作数据库）
-      handleFun(db, collections, selector, fn);
-      db.close();
-  });
+
+
+function handleDb(collections, selector, handleFun, fn) {
+    MongoClient.connect(Urls, function (err, db) {
+        assert.equal(null, err);
+        console.log("Connected correctly to server");
+        // 根据 请求的地址来确定是什么操作  （为了安全，避免前端直接通过请求url操作数据库）
+        handleFun(db, collections, selector, fn);
+        db.close();
+    });
 };
+
+//分页查询
+function findByPagination(collections, selector, page, count, fn) {
+    let skipnumber = (page - 1) * count;
+    let totalCount;
+    MongoClient.connect(Urls, function (err, db) {
+        let collection = db.collection(collections);
+        assert.equal(null, err);
+        console.log("Connected correctly to server");
+
+        collection.find(selector).count((err, res) => {
+            assert.equal(err, null);
+            totalCount = res;
+        });
+        // 根据 请求的地址来确定是什么操作  （为了安全，避免前端直接通过请求url操作数据库）
+        collection.find(selector).skip(skipnumber).limit(count).toArray(function (err, result) {
+            try {
+                assert.equal(err, null);
+                fn({
+                    success: true,
+                    data: result,
+                    totalCount: totalCount
+                });
+            } catch (e) {
+                console.log(e);
+                fn({
+                    success: false,
+                    data: [],
+                    totalCount: totalCount
+                });
+            }
+            
+        });
+        db.close();
+    });
+}
 
 module.exports = router;
