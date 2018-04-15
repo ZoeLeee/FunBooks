60f12d08505a2654f104a7256502165212ad083f
Zoe <33168975+ZoeLeee@users.noreply.github.com>
2018-04-15 21:05:53 +0800

    代码清理

--- app.js
+++ app.js
@@ -1,8 +1,5 @@
 var express = require('express');
-var http=require('http');
 var session=require('express-session');
-
-
 var path = require('path');
 var logger = require('morgan');
 
@@ -10,7 +7,7 @@ var indexRouter = require('./routes/myroutes');
 
 var app = express();
 app.all('*',(req,res,next)=>{
-  res.header("Access-Control-Allow-Origin","http://localhost:3000");
+  res.header("Access-Control-Allow-Origin","*");
   res.header('Access-Control-Allow-Methods','PUT,GET,POST,DELETE,OPTIONS');
   res.header("Access-Control-Allow-Headers", "X-Requested-With");
   res.header('Access-Control-Allow-Headers','Content-Type');
@@ -26,9 +23,6 @@ app.use(session({
   resave:true,
   saveUninitialized:false
 }))
-// view engine setup
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'jade');
 
 //输出服务器记录
 app.use(logger('dev'));
@@ -41,17 +35,6 @@ app.use('/', indexRouter);
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
   next(createError(404));
-});
-
-// error handler
-app.use(function(err, req, res, next) {
-  // set locals, only providing error in development
-  res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
-
-  // render the error page
-  res.status(err.status || 500);
-  res.render('error');
 });
 
 app.listen(3000,()=>{
